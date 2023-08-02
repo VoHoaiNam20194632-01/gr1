f3c78f01154d7873df68a2ba92f4eddee792c6c0
VoHoaiNam20194632-01 <nam.vh194632@sis.hust.edu.vn>
2023-08-03 00:25:37 +0700

    search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,24 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss'
-
-const cx = classNames.bind(styles)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.module.scss';
+const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-
+                <div className={cx('logo')}>NAMIT</div>
+                <div className={cx('search')}>
+                    <input placeholder='Search Course' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
             </div>
         </header>
 
